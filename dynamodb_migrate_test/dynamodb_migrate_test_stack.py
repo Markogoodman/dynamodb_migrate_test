c32690965d07906e731628be1a116f030d35e967
Marko Peng <marko958m@gmail.com>
2021-12-29 22:53:13 +0800

    go

--- dynamodb_migrate_test/dynamodb_migrate_test_stack.py
+++ dynamodb_migrate_test/dynamodb_migrate_test_stack.py
@@ -1,11 +1,14 @@
 from constructs import Construct
 from aws_cdk import (
     Duration,
+    RemovalPolicy,
+    RemovalPolicyOptions,
     Stack,
     aws_iam as iam,
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
+    aws_dynamodb as dynamodb
 )
 
 
@@ -24,3 +27,49 @@ class DynamodbMigrateTestStack(Stack):
         )
 
         topic.add_subscription(subs.SqsSubscription(queue))
+
+        p = RemovalPolicy(RemovalPolicy.RETAIN)
+        table = dynamodb.Table(self, "markotestsecond",
+            partition_key=dynamodb.Attribute(name="name", type=dynamodb.AttributeType.STRING),
+            sort_key=dynamodb.Attribute(name="age", type=dynamodb.AttributeType.STRING),
+            removal_policy=p,
+            stream=dynamodb.StreamViewType(dynamodb.StreamViewType.NEW_IMAGE)
+        )
+
+
+        # global_secondary_index_props = dynamodb.GlobalSecondaryIndexProps(
+        #     index_name="hellp",
+        #     partition_key=dynamodb.Attribute(
+        #         name="id111",
+        #         type=dynamodb.AttributeType.BINARY
+        #     ),
+
+        #     # the properties below are optional
+        #     non_key_attributes=["nonKeyAttributes"],
+        #     projection_type=dynamodb.ProjectionType.KEYS_ONLY,
+        #     read_capacity=1,
+        #     sort_key=dynamodb.Attribute(
+        #         name="id222",
+        #         type=dynamodb.AttributeType.BINARY
+        #     ),
+        #     write_capacity=1
+        # )
+
+
+        table.add_global_secondary_index(index_name="name-index",
+            partition_key=dynamodb.Attribute(
+                name="name",
+                type=dynamodb.AttributeType.STRING
+            ),
+            read_capacity=1,
+            write_capacity=1)
+        # "CDKMetadata": {
+        # "Type": "AWS::CDK::Metadata",
+        # "Properties": {
+        #     "Analytics": "v2:deflate64:H4sIAAAAAAAA/1WNQQ6CMBBFz8K+HVE27rkAgnsDpcYR7GCnjSFN7y4tiYmb+f+/vGROUEFZ9B+WapzkjAOEzvVqEhu6BX4zhIvXXov6bvaSb0MzqvUH9xkFm83v/MDK4uKQTDL+9pUWVInmEmOqrWbyVuUfNZkRkxlFs7oHmUMFZziWxZMRpfXG4UtDu+cXpUJN470AAAA="
+        # },
+        # "Metadata": {
+        #     "aws:cdk:path": "dynamodb-migrate-test/CDKMetadata/Default"
+        # },
+        # "Condition": "CDKMetadataAvailable"
+        # }
